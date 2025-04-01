02625ff86b0fbfb5ddf2286915290052b03018e1
Vitalii Vasylets <mohara88s@gmail.com>
2025-04-01 10:07:49 +0300

    add suggester comands

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from utility import *
 from colorama import Fore, init
+import difflib
 
 init(autoreset=True)
+commands = ["hello","add","change","delete","phone","all","add-birthday","show-birthday","birthdays"]
+
+def suggest_command(user_command):
+    matches = difflib.get_close_matches(user_command, commands, n=1, cutoff=0.6)
+    return matches[0] if matches else None
 
 def main():
     book = load_data()
@@ -11,7 +17,6 @@ def main():
             user_input = input(f"Enter a command: {Fore.LIGHTCYAN_EX}")
             print(Fore.RESET, end="")
             command, *args = parse_input(user_input)
-        
             if command in ["close", "exit"]:
                 save_data(book)
                 print("Good bye!")
@@ -39,7 +44,12 @@ def main():
                 for i, contact in enumerate(birthdays(book)):
                     print(colorize_message(f"{contact.name:<20}{contact.birthday}", f"{"CYAN" if i%2==0 else "BLUE"}"))
             else:
-                print(colorize_message("Invalid command", "YELLOW"))
+                suggestion = suggest_command(command)
+                if suggestion:
+                    print(colorize_message(f"Unknown command: '{command}'. Did you mean '{suggestion}'?", "YELLOW"))
+
+                else:
+                    print(colorize_message(f"Invalid command: '{command}'.", "YELLOW"))  
         except Exception as e:
         # Обробка будь-якого винятку
             error_handler(e)
